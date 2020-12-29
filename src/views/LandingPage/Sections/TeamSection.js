8d8c90a04ff3b98ed1fec824e3c9c1f9a7ff82ca
sscs12345 <shauryashah98@gmail.com>
2020-12-29 13:46:38 +0530

    Update TeamSection.js

--- src/views/LandingPage/Sections/TeamSection.js
+++ src/views/LandingPage/Sections/TeamSection.js
@@ -40,14 +40,14 @@ export default function TeamSection() {
                 <img src={team1} alt="..." className={imageClasses} />
               </GridItem>
               <h4 className={classes.cardTitle}>
-                Gigi Hadid
+                Shaurya Shah
                 <br />
                 <small className={classes.smallTitle}>Model</small>
               </h4>
               <CardBody>
                 <p className={classes.description}>
-                  You can write here details about one of your team members. You
-                  can give more details about what they do. Feel free to add
+                  BSC IT Graduate from Mumbai, IN
+                  CPL holder done from Vancouver, CA
                   some <a href="#pablo">links</a> for people to be able to
                   follow them outside the site.
                 </p>
