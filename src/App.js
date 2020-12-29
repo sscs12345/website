7d7e5167ade13c1dc121003234041864c71ab74e
sscs12345 <shauryashah98@gmail.com>
2020-12-29 12:00:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Coming soon 1st Jan 2021
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://npchosting.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          NPCHOSTING.COM
         </a>
       </header>
     </div>
