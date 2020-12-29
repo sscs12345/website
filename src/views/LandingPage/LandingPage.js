b66716fc7b4885ea880a45f1080bfd7ceea9da98
sscs12345 <shauryashah98@gmail.com>
2020-12-29 13:30:02 +0530

    Update LandingPage.js

--- src/views/LandingPage/LandingPage.js
+++ src/views/LandingPage/LandingPage.js
@@ -34,7 +34,7 @@ export default function LandingPage(props) {
       <Header
         color="transparent"
         routes={dashboardRoutes}
-        brand="Material Kit React"
+        brand="NPC HOSTING"
         rightLinks={<HeaderLinks />}
         fixed
         changeColorOnScroll={{
@@ -47,18 +47,18 @@ export default function LandingPage(props) {
         <div className={classes.container}>
           <GridContainer>
             <GridItem xs={12} sm={12} md={6}>
-              <h1 className={classes.title}>Your Story Starts With Us.</h1>
+              <h1 className={classes.title}>Host your next gameserver in the cloud with us.</h1>
               <h4>
-                Every landing page needs a small description after the big bold
-                title, that{"'"}s why we added this text here. Add here all the
-                information that can make you or your product create the first
-                impression.
+                Indian Datacenter - Mumbai & Hyderabad
+                Gameserver Panel
+                Free DDOS Protection
+                24/7 Support
               </h4>
               <br />
               <Button
                 color="danger"
                 size="lg"
-                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ref=creativetim"
+                href="https://www.youtube.com/watch?v=anANeBaEw0s"
                 target="_blank"
                 rel="noopener noreferrer"
               >
